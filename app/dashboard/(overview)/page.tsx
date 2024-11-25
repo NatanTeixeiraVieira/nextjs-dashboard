1f65b1dbb8c5f4fed3e7278def91f7bdb0accaba
NatanTeixeiraVieira <natanteixeiravieira@gmail.com>
2024-11-24 22:52:27 -0300

    feat: Add metadata

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -1,18 +1,21 @@
-import CardWrapper, { Card } from '@/app/ui/dashboard/cards';
+import CardWrapper from '@/app/ui/dashboard/cards';
 import RevenueChart from '@/app/ui/dashboard/revenue-chart';
 import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
 import { lusitana } from '@/app/ui/fonts';
-import {
-  fetchCardData,
-  // fetchLatestInvoices,
-  // fetchRevenue,
-} from '../../lib/data';
+import // fetchLatestInvoices,
+// fetchRevenue,
+'../../lib/data';
 import { Suspense } from 'react';
 import {
   CardSkeleton,
   LatestInvoicesSkeleton,
   RevenueChartSkeleton,
 } from '@/app/ui/skeletons';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Dashboard',
+};
 
 export default async function Page() {
   // const revenue = await fetchRevenue();
